9c19bc4c1718065b431adaa93675acd88c375841
Tuan Pham <tuanpham.developer@gmail.com>
2020-08-17 09:37:31 -0400

    Updates

---
+++ precache-manifest.ab28b8741d3c18461a0c0eef2e889cdd.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a377713764b82cf3493b349668a80f17",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "2fb4d327957f282dc698",
+    "url": "/react-tic-tac-toe/static/js/2.5824dc12.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-tic-tac-toe/static/js/2.5824dc12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05c30f11aa92773a7d26",
+    "url": "/react-tic-tac-toe/static/js/main.24182cda.chunk.js"
+  },
+  {
+    "revision": "d9255e6de55285e18dc3",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.81654cbd.js"
+  },
+  {
+    "revision": "dfac110038a9155e8e269ee1411bccd5",
+    "url": "/react-tic-tac-toe/static/media/check.dfac1100.svg"
+  },
+  {
+    "revision": "c25c79a518f17243161a5cd54fabedf9",
+    "url": "/react-tic-tac-toe/static/media/circle.c25c79a5.svg"
+  },
+  {
+    "revision": "45b40632e7db466044bf6159348e6251",
+    "url": "/react-tic-tac-toe/static/media/config.45b40632.svg"
+  },
+  {
+    "revision": "fc1cb63c74abcec13c08c7ee2417935f",
+    "url": "/react-tic-tac-toe/static/media/cross.fc1cb63c.svg"
+  },
+  {
+    "revision": "b9aa6cb235864d4a7d95e2d973453680",
+    "url": "/react-tic-tac-toe/static/media/restart.b9aa6cb2.svg"
+  },
+  {
+    "revision": "ce62d8529bfb46f4b62d1be751449c12",
+    "url": "/react-tic-tac-toe/static/media/tie.ce62d852.svg"
+  }
+]);
